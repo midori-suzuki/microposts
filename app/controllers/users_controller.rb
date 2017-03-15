8fec86279bfc4b07c1ab9edc29f763d03c742215
midori <midori-ito@outlook.com>
2017-03-15 15:12:10 +0000

    fix typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,11 +21,11 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-
+  
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:success] = "Updated your Pofile"
+      flash[:success] = "Updated your Plofile"
       redirect_to @user
     else
       render 'edit'
